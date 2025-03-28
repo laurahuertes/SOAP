4c7f46b2710b7683db4697a44a8c91e818f284cc
Laura Reyes <laura.reyes@babelgroup.com>
2025-03-28 13:01:49 +0100

    commit cliente

--- soapclient/src/main/java/es/neesis/soapclient/client/UserClient.java
+++ soapclient/src/main/java/es/neesis/soapclient/client/UserClient.java
@@ -4,6 +4,9 @@ import es.neesis.soapclient.ws.user.GetUserRequest;
 import es.neesis.soapclient.ws.user.GetUserResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class UserClient extends WebServiceGatewaySupport {
 
     public GetUserResponse getUser(int id) {
@@ -12,4 +15,24 @@ public class UserClient extends WebServiceGatewaySupport {
 
         return (GetUserResponse) getWebServiceTemplate().marshalSendAndReceive(request);
     }
+
+    public AuthenticateResponse authenticateUser(String username, String password) {
+        String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
+
+        AuthenticateRequest request = new AuthenticateReuqest();
+        request.setUsername(username);
+        request.setPassword(encodedPassword);
+
+        AuthenticateResponse response = (GetUserResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+
+        if ("OK".equals(response.getStatus())) {
+            System.out.println("Login correcto. Usuario: " + response.getUser().getUsername());
+            callNumbersToWordsService(response.getUser().getEmail());
+        } else {
+            System.out.println("Error: " + response.getMessage());
+        }
+
+        return response;
+    }
+
 }
